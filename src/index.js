d823c3e4a14b4c15f97d4eb9173da9fc4dcd590f
RanSharon99 <ransharon23@gmail.com>
2024-05-03 17:43:52 +0300

    skeleton project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+ const myRoot = document.getElementById("root")
+
+ const root = ReactDOM.createRoot(myRoot)
+;
+ const title =  <h1 style ={{color: 'green'}}> hello world</h1>
+
+ root.render(title)
